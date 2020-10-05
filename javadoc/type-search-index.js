46c142ef8b2bd2ffeefb47c1a164e8d264bdcb98
C-Berry98 <50661278+C-Berry98@users.noreply.github.com>
2020-10-05 15:59:06 +0100

    Demo

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"Week1","l":"HelloWorld"}]
---
+++ src/Week1/HelloWorld.java
@@ -0,0 +1,16 @@
+package Week1;
+
+/**
+ * A demo class
+ * @author Curtis Berry
+ * @version September 2020
+ */
+public class HelloWorld {
+    /**
+     * A demo method
+     * @param args command line arguments which are not used
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
